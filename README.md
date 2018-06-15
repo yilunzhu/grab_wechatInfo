498176c6673145faa16891decaff7a24d83a4418
zhudalun1553 <allen.yl.zhu@gmail.com>
2018-06-15 10:23:01 +0800

    new function: geographical distribution of all the friends

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
 # grab_wechatInfo
 This script uses itchat module to achieve three basic functions: 
 
-1. send message. Inputted the remark name and the message, the function will send it to that person. If the input is "" or the it equals to the user's name, the function will send the message to the user. Later, send image/file will be added to this script.
+# Send message. 
+```python
+python grab_wechat.py send
+```
+Inputted the remark name and the message, the function will send it to that person. If the input is "" or the it equals to the user's name, the function will send the message to the user. Later, send image/file will be added to this script.
 
-2. collect sex info. The function collects the number of male, female, and other (unspecified) and compute the proportion of each sex.
+# Collect gender info. 
+```python
+python grab_wechat.py gender
+```
+The function collects the number of male, female, and other (unspecified) and compute the proportion of each sex.
 
-3. collect "what's up" info from all the friends of the user you log in. It returns the 30 most frequent words (to simplify and avoid function words, I only collect nouns, adjectives for Chinese and nouns, verbs, and adverbs for English) from Chinese and English. Though people sometimes use emoji or other languages in their "What's up" like Korean, French, etc., I narrows the language down to Chinese and English in order to decrease the difficulty of pos tagging. The function uses THULAC http://thulac.thunlp.org/ to split Chinese sentences and tag Chinese words. For English, I use NLTK to do tokenize and pos tagging.
+# Collect geographical distribution of your wechat friends. (China only)
+```python
+python grab_wechat.py map
+```
+
+# collect "what's up" info from all of your friends. 
+```python
+python grab_wechat.py signature
+```
+It returns the 30 most frequent words (to simplify and avoid function words, I only collect nouns, adjectives for Chinese and nouns, verbs, and adverbs for English) from Chinese and English. Though people sometimes use emoji or other languages in their "What's up" like Korean, French, etc., I narrows the language down to Chinese and English in order to decrease the difficulty of pos tagging. The function uses THULAC http://thulac.thunlp.org/ to split Chinese sentences and tag Chinese words. For English, I use NLTK to do tokenize and pos tagging.
 
--- grab_wechat.py
+++ grab_wechat.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 import itchat, sys, re
-from collections import defaultdict
+from collections import defaultdict, Counter
 import thulac, nltk
 from langdetect import detect
-from pyecharts import Pie
+from pyecharts import Pie, WordCloud, Map
 
 
 def send(user_dict):
@@ -76,6 +76,7 @@ if __name__ == '__main__':
     itchat.auto_login(hotReload=True)
     # itchat.login()
     user_dict = itchat.get_friends()
+    username = user_dict[0]['NickName']
     if len(sys.argv) <= 1:
         assert False, "Specify which function to use: 'send' or 'sex' or 'signature'"
     opt = sys.argv[1]
@@ -87,16 +88,38 @@ if __name__ == '__main__':
         print("好友总数=%s" % int(total))
         print("男性好友=%s，女性好友=%s，未标明性别好友=%s" % (male, female, other))
         print("男性好友占比=%s，女性好友占比=%s" % (male / total, female / total))
-        username = user_dict[0]['NickName']
         pie = Pie("微信用户%s性别分布" % username, title_pos="center")
         attr = ['男性好友', '女性好友', '未标明性别好友']
         v1 = [male, female, other]
         pie.add("", attr, v1, radius=[40, 75], rosetype="radius", is_label_show=True, legend_orient='vertical',
                 legend_pos='left')
-        # pie.render(path="%s.png" % username)
-        pie.render()
+        pie.render(path='%s_gender.html' % username)
 
     elif opt == "signature":
         signature(user_dict)
+
+        # wordcloud
+        '''wordcloud = WordCloud(width=1300, height=620)
+        wordcloud.add("", name, value, word_size_range=[30, 100],
+                      shape='diamond')
+        wordcloud.render()'''
+    elif opt == "map":
+        region_list = Counter()
+        for friend in user_dict[1:]:
+            search = re.search('[a-zA-Z]', friend['Province'])
+            region = friend['Province']
+            if search is None:
+                if len(region) > 0:
+                    region_list[region] += 1
+        # print(region_list)
+        max_number = max(region_list.values())
+        number = (max_number // 50 + 1) * 50 if (max_number % 50) != 0 else max_number
+        wechat_map = Map("%s微信好友地区分布" % username, width=1200, height=600)
+        attr = [region for region in region_list.keys()]
+        value = [v for v in region_list.values()]
+        wechat_map.add("", attr, value, maptype='china', is_label_show=True, visual_range=[0, number], symbol_size=15,
+                       is_visualmap=True)
+        wechat_map.render(path='%s_map.html' % username)
+
     else:
         assert False, "Invalid command line argument"
